dec828ccc7f438ad917a65e4c478de835488aa0d
Stephanie <yangcao@redhat.com>
2024-05-15 14:31:02 -0400

    add catalog-info to gitops template
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 
 # get app templates
-$ROOTDIR/scripts/import-ai-app-template
+$ROOTDIR/scripts/import-gitops-template
 $ROOTDIR/scripts/import-ai-lab-samples
